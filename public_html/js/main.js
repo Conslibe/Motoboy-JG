c1a240912a369831b070c178703262b0b0288cc1
Caio Milleo <caio_milleo@hotmail.com>
2017-07-12 23:35:00 -0300

    nova img e efeitos

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,10 @@
-$('.texto').fadeOut(10000,function() {
-      $('.motoboy-efeito').fadeIn('slow', function() {
-            $('.moto').animate({"left":"119px"});
-      });
+$('.texto').fadeOut(10000, function () {
+    $('.moto').fadeIn(2000);
+    $('.esquerda').fadeIn(2000);
+    $('.peca1').fadeIn(3000);
+    $('.peca2').fadeIn(4000);
+    $('.peca3').fadeIn(5000);
+    $('.peca4').fadeIn(6000);
+
+
 });
